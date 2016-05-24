1f5805868d8779bbfcdd3aaac96609e4784eb442
Christopher Davenport <ChristopherDavenport@outlook.com>
2016-05-24 17:04:01 -0400

    Moved in Class For Cleaner Reading

--- src/main/scala/Tables/IdentRecord.scala
+++ src/main/scala/Tables/IdentRecord.scala
@@ -13,6 +13,44 @@ case class IdentRecord(
                         facultyInfo: FacultyInfo,
                         studentInfo: StudentInfo
                       )
+case class PersonalInfo(
+                         EckerdId: Option[String],
+                         FirstName: Option[String],
+                         LastName: Option[String],
+
+                         Email: Option[String]
+                       )
+
+case class BussinessInfo(
+                          EnterpriseUsername: Option[String],
+
+                          EmployeeClass: Option[String],
+                          EmployeeStatus: Option[String],
+                          HomeOrg: Option[String],
+
+                          JobOrg: Option[String],
+                          JobPosn: Option[String],
+
+                          Roles: Option[String]
+                        )
+
+case class FacultyInfo(
+                        FacultyStatus: Option[String],
+                        FacultyTag: Option[String],
+                        FacultySchdInd: Option[String],
+                        FacultyAdvisorInd: Option[String],
+
+                        FacultyType: Option[String]
+                      )
+
+case class StudentInfo(
+                        StudentStatus: Option[String],
+                        StudentLevel: Option[String],
+                        StudentClass: Option[String],
+
+                        Majors: Option[String],
+                        Minors: Option[String]
+                      )
 
 class IDENT_MASTER (tag: Tag) extends Table[IdentRecord](tag, "IDENT_MASTER") {
 
@@ -102,41 +140,3 @@ class IDENT_MASTER (tag: Tag) extends Table[IdentRecord](tag, "IDENT_MASTER") {
   def pidmIndex = index("pidm_index", Pidm, unique=true)
 }
 
-case class PersonalInfo(
-                         EckerdId: Option[String],
-                         FirstName: Option[String],
-                         LastName: Option[String],
-
-                         Email: Option[String]
-                       )
-
-case class BussinessInfo(
-                          EnterpriseUsername: Option[String],
-
-                          EmployeeClass: Option[String],
-                          EmployeeStatus: Option[String],
-                          HomeOrg: Option[String],
-
-                          JobOrg: Option[String],
-                          JobPosn: Option[String],
-
-                          Roles: Option[String]
-                        )
-
-case class FacultyInfo(
-                          FacultyStatus: Option[String],
-                          FacultyTag: Option[String],
-                          FacultySchdInd: Option[String],
-                          FacultyAdvisorInd: Option[String],
-
-                          FacultyType: Option[String]
-                        )
-
-case class StudentInfo(
-                        StudentStatus: Option[String],
-                        StudentLevel: Option[String],
-                        StudentClass: Option[String],
-
-                        Majors: Option[String],
-                        Minors: Option[String]
-                      )
